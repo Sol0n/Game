ca9a3a524ed9e9b276129e7b3c339dea5afa9d8e
Sol0n <poludennik@gmail.com>
2016-10-01 11:13:23 +0300

    commit

---
+++ js/common.js
@@ -0,0 +1,70 @@
+$(function() {
+  $('.1pair').click(function() {
+    var wrap = $('.content_1');
+    if($(wrap).css('-webkit-transform') == 'matrix(1, 0, 0, 1, 0, 0)') {
+      $(wrap).css({'-webkit-transform':'rotateY(180deg)'});
+    } else {
+      $(wrap).css({'-webkit-transform':'rotateY(0deg)'});
+    }
+  });
+});
+
+$(function() {
+  $('.2pair').click(function() {
+    var wrap = $('.content_2');
+    if($(wrap).css('-webkit-transform') == 'matrix(1, 0, 0, 1, 0, 0)') {
+      $(wrap).css({'-webkit-transform':'rotateY(180deg)'});
+    } else {
+      $(wrap).css({'-webkit-transform':'rotateY(0deg)'});
+    }
+
+  });
+});
+
+$(function() {
+  $('.3pair').click(function() {
+    var wrap = $('.content_3');
+    if($(wrap).css('-webkit-transform') == 'matrix(1, 0, 0, 1, 0, 0)') {
+      $(wrap).css({'-webkit-transform':'rotateY(180deg)'});
+    } else {
+      $(wrap).css({'-webkit-transform':'rotateY(0deg)'});
+    }
+
+  });
+});
+
+$(function() {
+  $('.4pair').click(function() {
+    var wrap = $('.content_4');
+    if($(wrap).css('-webkit-transform') == 'matrix(1, 0, 0, 1, 0, 0)') {
+      $(wrap).css({'-webkit-transform':'rotateY(180deg)'});
+    } else {
+      $(wrap).css({'-webkit-transform':'rotateY(0deg)'});
+    }
+
+  });
+});
+
+$(function() {
+  $('.5pair').click(function() {
+    var wrap = $('.content_5');
+    if($(wrap).css('-webkit-transform') == 'matrix(1, 0, 0, 1, 0, 0)') {
+      $(wrap).css({'-webkit-transform':'rotateY(180deg)'});
+    } else {
+      $(wrap).css({'-webkit-transform':'rotateY(0deg)'});
+    }
+
+  });
+});
+
+$(function() {
+  $('.6pair').click(function() {
+    var wrap = $('.content_6');
+    if($(wrap).css('-webkit-transform') == 'matrix(1, 0, 0, 1, 0, 0)') {
+      $(wrap).css({'-webkit-transform':'rotateY(180deg)'});
+    } else {
+      $(wrap).css({'-webkit-transform':'rotateY(0deg)'});
+    }
+
+  });
+});
